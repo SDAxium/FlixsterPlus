785ba6cb7864be9a14269b69284c4218e189c24f
SDAxium <sdaxium@gmail.com>
2022-10-01 02:24:39 -0400

    Simple Flixster Complete
    Created a simple app that displays movies by accessing The Movie Database API

---
+++ app/src/main/java/com/example/flixsterplus/MovieRecyclerViewAdapter.kt
@@ -0,0 +1,70 @@
+package com.example.flixsterplus
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.flixsterplus.R.id
+
+/**
+ * [RecyclerView.Adapter] that can display a [Movie] and makes a call to the
+ * specified [OnListFragmentInteractionListener].
+ */
+class MovieRecyclerViewAdapter(
+    private val movies: List<Movie>,
+    private val mListener: OnListFragmentInteractionListener?
+)
+    : RecyclerView.Adapter<MovieRecyclerViewAdapter.MovieViewHolder>()
+{
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.fragment_movie, parent, false)
+        return MovieViewHolder(view)
+    }
+
+    /**
+     * This inner class lets us refer to all the different View elements
+     * (Yes, the same ones as in the XML layout files!)
+     */
+    inner class MovieViewHolder(val mView: View) : RecyclerView.ViewHolder(mView) {
+        var mItem: Movie? = null
+        val mMovieTitle: TextView = mView.findViewById<View>(id.movie_title) as TextView
+        val mMovieDescription: TextView = mView.findViewById<View>(id.movie_description) as TextView
+        val mMoviePoster: ImageView = mView.findViewById(id.movie_poster) as ImageView
+
+        override fun toString(): String {
+            return mMovieTitle.toString() + " '" + mMovieDescription.text + "'"
+        }
+    }
+
+    /**
+     * This lets us "bind" each Views in the ViewHolder to its' actual data!
+     */
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        val movie = movies[position]
+
+        holder.mItem = movie
+        holder.mMovieTitle.text = movie.title
+        holder.mMovieDescription.text = movie.overview
+        Glide.with(holder.mView)
+            .load("https://image.tmdb.org/t/p/w500"+movie.posterUrl)
+            .centerInside()
+            .into(holder.mMoviePoster)
+
+        holder.mView.setOnClickListener {
+            holder.mItem?.let { movie ->
+                mListener?.onItemClick(movie)
+            }
+        }
+    }
+
+    /**
+     * Remember: RecyclerView adapters require a getItemCount() method.
+     */
+    override fun getItemCount(): Int {
+        return movies.size
+    }
+}
---
+++ app/src/main/java/com/example/flixsterplus/OnListFragmentInteractionListener.kt
@@ -0,0 +1,8 @@
+package com.example.flixsterplus
+
+/*
+ *  Stolen from lab
+ */
+interface OnListFragmentInteractionListener {
+    fun onItemClick(item: Movie)
+}
